c0e2bb1152d5f3afb8746e3465689c6edb390495
herode <binh.nx@workhouse.me>
2019-09-30 16:55:29 +0700

    Updates

---
+++ precache-manifest.f8a508b8b32b9d09a9b71c4d1539d008.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb6b06dff0edda64a857e87f940cd16",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "dede0fa0afc0ee9315b0",
+    "url": "/react-admin/static/css/main.ef8becb3.chunk.css"
+  },
+  {
+    "revision": "c446d76b3efad0a2afcf",
+    "url": "/react-admin/static/js/2.54467e02.chunk.js"
+  },
+  {
+    "revision": "dede0fa0afc0ee9315b0",
+    "url": "/react-admin/static/js/main.cfff7ca7.chunk.js"
+  },
+  {
+    "revision": "4f989c5c59420d262ac3",
+    "url": "/react-admin/static/js/runtime~main.f8761412.js"
+  },
+  {
+    "revision": "34fdc4136a7e7e129a6e1897b4c50ee1",
+    "url": "/react-admin/static/media/icomoon.34fdc413.ttf"
+  },
+  {
+    "revision": "9fc4031c3e96973f418ed77f0ff3b0f3",
+    "url": "/react-admin/static/media/icomoon.9fc4031c.svg"
+  },
+  {
+    "revision": "ad579ff1278c3d4388d15fc27b40b67e",
+    "url": "/react-admin/static/media/icomoon.ad579ff1.eot"
+  },
+  {
+    "revision": "c068d37f3b072da2ecefcf5369f219ad",
+    "url": "/react-admin/static/media/icomoon.c068d37f.woff"
+  },
+  {
+    "revision": "b44041725c60edeefab3b09e2e636efa",
+    "url": "/react-admin/static/media/login_cover.b4404172.jpg"
+  },
+  {
+    "revision": "75ea661ca13988a82642aa34c0fdaf4c",
+    "url": "/react-admin/static/media/logo.75ea661c.png"
+  },
+  {
+    "revision": "59e6f5b82adbc382f56107da63873a6e",
+    "url": "/react-admin/static/media/placeholder.59e6f5b8.jpg"
+  },
+  {
+    "revision": "372a2830643e759ed61f6a8590900192",
+    "url": "/react-admin/static/media/summernote.372a2830.eot"
+  },
+  {
+    "revision": "608fd10666a860567740497e84492928",
+    "url": "/react-admin/static/media/summernote.608fd106.woff"
+  },
+  {
+    "revision": "ce9ce49a0e1ca283fa242ba2cf8e24d9",
+    "url": "/react-admin/static/media/summernote.ce9ce49a.ttf"
+  },
+  {
+    "revision": "276508604117bfb3e66b4080688d68b8",
+    "url": "/react-admin/static/media/user_bg3.27650860.jpg"
+  }
+]);
